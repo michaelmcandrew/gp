1fdcf8be6e4a2b1e23bbabb47b496aa62d54a16a
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2010-12-13 19:13:28 +0000

    added 'user delete'; started ward party import/export; dd import improvements

--- modules/custom/gpew_dd_import/gpew_dd_import.batch.php
+++ modules/custom/gpew_dd_import/gpew_dd_import.batch.php
@@ -10,18 +10,19 @@ $config = CRM_Core_Config::singleton();
 // include api, drupal module, etc.
 require_once('gpew_dd_import.module');
 
+
+
 $ddms_to_be_imported=CRM_Core_DAO::executeQuery( "
 	SELECT *
 	FROM gp_data.ddm_all_without_cancelled
-	WHERE ConfirmationReference NOT IN (
-		SELECT direct_debit_reference_16 FROM ukgr_crm.civicrm_value_external_identifiers_5
-	)
 ");
+
+
 
 echo "Going to loop through {$ddms_to_be_imported->N} rows...
 ";
 
-$test=FALSE;
+$test=TRUE;
 $report=array();
 while($ddms_to_be_imported->fetch()){
 //	print_r((array)$ddms_to_be_imported);
