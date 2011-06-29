11ae77ff45eaee51a04ec85492fb473f06f080b3
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2011-06-29 18:47:41 +0100

    capitation now takes into account part membership payments

--- modules/custom/gpew_capitation/gpew_capitation.batch.php
+++ modules/custom/gpew_capitation/gpew_capitation.batch.php
@@ -1,6 +1,6 @@
 <?php
-require_once '/m/htdocs/gp/sites/all/modules/civicrm/civicrm.config.php';
-require_once('/m/htdocs/gp/sites/gp.local/civicrm.settings.php');
+require_once '/var/www/gp/sites/all/modules/civicrm/civicrm.config.php';
+require_once('/var/www/gp/sites/gp.local/civicrm.settings.php');
 require_once 'CRM/Core/Config.php';
 
 $config = CRM_Core_Config::singleton();
@@ -11,22 +11,19 @@ require_once('CRM/Core/DAO.php');
 ini_set('memory_limit', '1000M');
 
 $query = "
-SELECT cc.id AS id, contact_id, lower(source) AS frequency
+SELECT cc.id
 FROM civicrm_contribution AS cc
-JOIN ukgr_crm.civicrm_contribution_type AS cct ON cc.contribution_type_id=cct.id
-JOIN gp_data.is_numbers AS is_numbers ON cct.name= is_numbers.`Item Number`
-WHERE import = 'm' AND id NOT IN (SELECT entity_id FROM `civicrm_value_capitation_4`)
+WHERE contribution_type_id=2
 ";
 
 $result = CRM_Core_DAO::executeQuery( $query );
 
-echo "Updating area information.\n";
+print_r($result);
 
 while($result->fetch()){
-	if(gpew_capitation_is_current_member($result->contact_id)){	
-		gpew_capitation_capitate($result->id,$result->frequency);
-		echo '.';
-	}
+	gpew_capitation_capitate($result->id, 'look_up', FALSE);
+	echo $x++.':'.$result->id.'   ';
 }
+
 echo "Done.\n";
 ?>
