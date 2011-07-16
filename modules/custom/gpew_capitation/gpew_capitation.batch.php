df3954b5dcdef952c6df6971edbc3e638f13290e
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2011-07-16 17:34:17 +0100

    capitation now capitates as low wage rate membership amount when contribution is less than membership fee (or fractional amount thereof)

--- modules/custom/gpew_capitation/gpew_capitation.batch.php
+++ modules/custom/gpew_capitation/gpew_capitation.batch.php
@@ -11,9 +11,26 @@ require_once('CRM/Core/DAO.php');
 ini_set('memory_limit', '1000M');
 
 $query = "
-SELECT cc.id
+SELECT cc.id, contact_id
 FROM civicrm_contribution AS cc
-WHERE contribution_type_id=2
+JOIN civicrm_contribution_type AS cct ON cc.contribution_type_id=cct.id
+JOIN is_numbers ON is_numbers.`Item Number`=cct.name
+WHERE is_numbers.import='m'
+";
+
+$result = CRM_Core_DAO::executeQuery( $query );
+
+print_r($result);
+
+while($result->fetch()){
+	gpew_capitation_capitate($result->id, 'look_up', TRUE);
+	echo $x++.': '.$result->id.'('.$result->contact_id.')   ';
+	
+}
+$query = "
+SELECT cc.id, contact_id
+FROM civicrm_contribution AS cc
+WHERE cc.contribution_type_id=2
 ";
 
 $result = CRM_Core_DAO::executeQuery( $query );
@@ -22,7 +39,7 @@ print_r($result);
 
 while($result->fetch()){
 	gpew_capitation_capitate($result->id, 'look_up', FALSE);
-	echo $x++.':'.$result->id.'   ';
+	echo $x++.': '.$result->id.'('.$result->contact_id.')   ';
 }
 
 echo "Done.\n";
