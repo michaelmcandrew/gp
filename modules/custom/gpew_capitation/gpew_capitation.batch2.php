fda74eb546bdccb63750c5ff02a2ec7398e07dad
Michael McAndrew <michaelmcandrew@Michael-McAndrews-MacBook-Pro.local>
2011-07-18 11:22:45 +0100

    split capitation batch in two

--- modules/custom/gpew_capitation/gpew_capitation.batch2.php
+++ modules/custom/gpew_capitation/gpew_capitation.batch2.php
@@ -13,18 +13,18 @@ ini_set('memory_limit', '1000M');
 $query = "
 SELECT cc.id, contact_id
 FROM civicrm_contribution AS cc
-WHERE cc.contribution_type_id=2
+JOIN civicrm_contribution_type AS cct ON cc.contribution_type_id=cct.id
+JOIN is_numbers ON is_numbers.`Item Number`=cct.name
+WHERE is_numbers.import='m'
 ";
-
-
 
 $result = CRM_Core_DAO::executeQuery( $query );
 
 print_r($result);
 
 while($result->fetch()){
-	gpew_capitation_capitate($result->id, 'look_up', FALSE);
-	echo $x++.': '.$result->id.'('.$result->contact_id.')   ';
+	gpew_capitation_capitate($result->id, 'look_up', TRUE);
+	echo $x++.': '.$result->id.'('.$result->contact_id.')   ';	
 }
 
 echo "Done.\n";
