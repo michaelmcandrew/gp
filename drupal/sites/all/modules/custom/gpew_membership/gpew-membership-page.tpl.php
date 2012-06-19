1b231cfa7c282f5e06ebb5fb0e0d931cdc4721fa
Peter McAndrew <petermcandrew@thirsectordesign.org>
2012-06-19 16:00:59 +0100

    adding info about payment freq. and changing more information link

--- drupal/sites/all/modules/custom/gpew_membership/gpew-membership-page.tpl.php
+++ drupal/sites/all/modules/custom/gpew_membership/gpew-membership-page.tpl.php
@@ -1,14 +1,16 @@
-<p>Your membership is:</br><b><?php echo $membership['membership_name']; ?></b></br>
-	Start date:  <b><?php echo $membership['start_date']; ?></b></br>
+<p><p>My membership type is:</br><b><?php echo $membership['membership_name']; ?></b></br></p>
 	<!--  TEMP REMOVED UNTIL 1950 ERROR IS FIXED WITH MEMBERSHIPS
+	Start date:  <b><?php echo $membership['start_date']; ?></b></br>
+	-->
 	<?php if ($membership['end_date']){ ?>
-	End date:  <b><?php echo $membership['end_date']; ?></b></br>
-	<?php } ?>-->
+	<p>End date:  <b><?php echo $membership['end_date']; ?></b></br></p>
+	<?php } ?>
 
-	<?php if ($is_primary) : ?>
-	<p><a href='<?php echo $renew_link; ?>'>To renew your <?php echo $membership['membership_name']; ?> membership click here.</a></p>
-	<?php else : ?>
-	<p><b>You cannot renew this joint membership, the primary member must be logged in.</b></p>
+	<?php if ($is_primary && ($membership['status_id'] == 3)) : ?>
+	<p><b>Your <?php echo $membership['membership_name']; ?> membership is in grace, <a href='<?php echo $renew_link; ?>'> to renew click here.</a></b></p>
+	<?php elseif(($membership['status_id'] == 3) && ($is_joint ==  7)) : ?>
+	<p><b>Your <?php echo $membership['membership_name']; ?> membership is in grace, to renew this membership the primary member must logged in.</b></p>
 	<?php endif; ?>
+	<p>My payment frequency is:  <b><?php echo $membership_payment_frequency; ?></b></p>
 	<?php echo $more_info; ?>
 	</p>
