01497d89b5ecfd36cdfced6e1af4af2c235be676
Peter McAndrew <petermcandrew@thirsectordesign.org>
2012-06-20 18:19:02 +0200

    adding all changes for membership information

--- drupal/sites/all/modules/custom/gpew_membership/gpew-membership-page.tpl.php
+++ drupal/sites/all/modules/custom/gpew_membership/gpew-membership-page.tpl.php
@@ -1,16 +1,48 @@
-<p><p>My membership type is:</br><b><?php echo $membership['membership_name']; ?></b></br></p>
+<p>My membership type is <b><?php echo $membership['membership_name']; ?></b>
 	<!--  TEMP REMOVED UNTIL 1950 ERROR IS FIXED WITH MEMBERSHIPS
 	Start date:  <b><?php echo $membership['start_date']; ?></b></br>
 	-->
-	<?php if ($membership['end_date']){ ?>
-	<p>End date:  <b><?php echo $membership['end_date']; ?></b></br></p>
-	<?php } ?>
+	<?php
+	
+	//need this line until the logic below is finished
+	$date_text = "ends on";
 
-	<?php if ($is_primary && ($membership['status_id'] == 3)) : ?>
-	<p><b>Your <?php echo $membership['membership_name']; ?> membership is in grace, <a href='<?php echo $renew_link; ?>'> to renew click here.</a></b></p>
-	<?php elseif(($membership['status_id'] == 3) && ($is_joint ==  7)) : ?>
-	<p><b>Your <?php echo $membership['membership_name']; ?> membership is in grace, to renew this membership the primary member must logged in.</b></p>
-	<?php endif; ?>
-	<p>My payment frequency is:  <b><?php echo $membership_payment_frequency; ?></b></p>
-	<?php echo $more_info; ?>
-	</p>
+	if ( /*current date is greater than end date*/ ) {
+      //$date_text = "ended on";
+  }	
+  else{
+   // $date_text = "ends on";
+  }
+	?>
+	<?php if ($membership['end_date']){ ?>
+	and <?php echo $date_text; ?> <b><?php echo $membership['end_date']; ?></b><?php } ?>.</p>
+
+	<?php if ($membership_payment_is_recurring == 1) { ?>
+  	<p>I'm currently paying by <b>direct debit/standing order</b>.</p>
+	  <p>My payment frequency is <b><?php echo strtolower($membership_payment_frequency); ?></b>.</p>
+	<?php } ?>
+  
+  <?php if ($membership_status_name) { ?>
+  	<p>My membership status is <b><?php echo strtolower($membership_status_name); ?></b>.
+	<?php } ?>
+	
+	<?php
+	if ($is_primary){
+	  $renew_text="<a href='<?php echo $renew_link; ?>'>To renew click here.</a>";
+	}
+	else{
+	  "The primary member must be logged in to renew this membership.";
+	}
+	?>
+  <?php if ($membership['status_id'] == 3) {
+    echo "You should renew your membership as soon as possible.";
+    echo "  $renew_text";
+    } ?>
+  <?php if ($membership['status_id'] == 4) {
+    echo "Your must renew this to continue to access content on the members' site.";
+    echo "  $renew_text";
+    } ?>
+  </p>
+  <p><?php echo $more_info; ?></p>
+
+	<!--  Split membership to first check status ID then write a nice sentence for each.	-->
